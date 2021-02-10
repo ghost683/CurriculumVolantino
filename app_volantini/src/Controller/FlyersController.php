d2f8f85bd42ac2b26ca94a4b8d86ba2a57783650
stefano <stefano.grado@gmail.com>
2021-02-10 19:55:07 +0100

    removed unused stuff, added fields filter logic

--- app_volantini/src/Controller/FlyersController.php
+++ app_volantini/src/Controller/FlyersController.php
@@ -25,8 +25,9 @@ class FlyersController extends AppController
     {
         $page = $this->request->getQuery('page');
         $filters = $this->request->getQuery('filter');
+        $fields = $this->request->getQuery('fields');
 
-        $this->set('volantini', $this->readCsv($filters));
+        $this->set('volantini', $this->readCsv($filters, explode(',', $fields)));
         $this->viewBuilder()->setOption('serialize', ['volantini']);
     }
 
@@ -39,18 +40,37 @@ class FlyersController extends AppController
     }
 
 
-    private function readCsv($filters, $page = 1, $limit = 10){
+    /**
+     * Working on function
+     * andrà spostata nelle utils
+     */
+    private function readCsv($filters, $fields, $page = 1, $limit = 10){
         
         $row = 1;
         $added = 0;
         $res = [];
-        if (($handle = fopen("C:/xampp/htdocs/appvolantini/app_volantini/webroot/flyers_data.csv", "r")) !== FALSE) {
+        $headersCsvIndexes = [];
+        if (($handle = fopen("C:/xampp/htdocs/appvolantini/CurriculumVolantino/app_volantini/webroot/flyers_data.csv", "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE && $added <= $limit) {
-                $num = count($data);
                 $row++;
                 if(trim($data[0]) !== '' && $data[0] !== 'id'){
-                    $res []= $data;
+                    $row = [];
+                    if($fields !== null ){ // manca controllo su field accettati... TODO
+                        foreach($headersCsvIndexes as $index){
+                            $row []= $data[$index];
+                        }
+                    }else {
+                        $row = $data;
+                    }
+
+                    $res []= $row;
                     $added++;
+                    
+                }else if($data[0] == 'id' && $fields !== null){ //temporaneo, la lettura del file è da rifattorizzare.
+                    //mi salvo gli indici posizionali dei campi richiesti
+                    foreach($fields as $field){
+                        $headersCsvIndexes []= array_search($field, $data);
+                    }
                 }
             }
             fclose($handle);
